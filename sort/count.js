7a4255f929f43ac04e57d9e26664a9cde08265d1
Roy <nirmalya.roy@capgemini.com>
2019-09-06 15:49:00 +0530

    Signed-off-by: Roy <nirmalya.roy@capgemini.com>

--- sort/count.js
+++ sort/count.js
@@ -1,7 +1,24 @@
 const swap = require("./swap.js");
 // import swap from './swap.js'
-const strandSort = (arr)=>{
+const countSort = (arr)=>{
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    let i, z = 0, count = [];
+
+    for (i = min; i <= max; i++) {
+        count[i] = 0;
+    }
+ 
+    for (i=0; i < arr.length; i++) {
+        count[arr[i]]++;
+    }
+ 
+    for (i = min; i <= max; i++) {
+        while (count[i]-- > 0) {
+            arr[z++] = i;
+        }
+    }
     return arr;
 }
-// export default strandSort;
-module.exports = strandSort;
+// export default countSort;
+module.exports = countSort;
